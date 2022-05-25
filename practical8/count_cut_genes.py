c09a2404166367bb9a3f26990cc72451769dd9a9
Minory654 <101198472+Minory654@users.noreply.github.com>
2022-05-25 08:03:46 +0800

    Rewrite the program

--- practical8/count_cut_genes.py
+++ practical8/count_cut_genes.py
@@ -1,22 +1,35 @@
 import re
-b=open("cut_genes.fa")          #import file containing sequence that can be cut by EcoRI
-
-print("Input the filename for the outcome of adding EcoRI to sequence:")
+print("Input the filename for the outcome of adding EcoRI to sequence: (Remember adding .fa behind the name)")
 h=input()                       #give name of the output format
 new=open(h,"w")                 #create output file
 
+b=open("Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa")          #import given fasta file
 har=b.read()
 clear=re.sub(r"\n","",har)      #clear "\n" of the file
+z = re.findall(r'[AGCT]{10,}',clear) #create list "z" that encodes all the raw sequence
+length=[]                            #create a list of length of the sequence
+
+n=[]                        #The order of sequence in the original file
+y=[]
+for i in range(len(z)):
+    if z[i].find("GAATTC")>=0:
+        n.append(i)
+        length.append(len(z[i]))     #search sequence that can be cut by EcoRI
+        y.append(z[i])
+
+names=re.findall(r"(>.+?) ",clear)
+
 
 n=[]                            #The fragments created by EcoRI
 a=0                             #The number of fragments
 
-z = re.findall(r'[AGCT]{10,}',clear) #create list "z" that encodes all the raw sequence
 
-names=re.findall(r"(>.+?) ",clear)
+
+
+
 for i in range(len(z)):
     a=z[i].find("GAATTC",)
-    n.append(a+2)
+    n.append(a+2)          #number of fragments
 for i in range(len(z)):
     new.write(names[i]+"    ")
     new.write("fragments'number after cutting by EcoRI: ")
